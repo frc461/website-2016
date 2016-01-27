9996d3bbd9881f43e5665a8d3baf17d7cc466fa5
BColliesRSmart <bordercolliesaresmart@gmail.com>
2016-01-27 16:55:26 -0500

    Added Carosual(idk how to spell that) background and sponsors on the bottom

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
 //= require_tree .
+$(function() {
+$('.carousel').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  adaptiveHeight: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+});
+});
