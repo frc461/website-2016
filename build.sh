4fe902126e93b7e3906bf546ed94af8e225f6d7e
Brad Thompson <thompsonb@wl.k12.in.us>
2016-04-23 11:45:11 -0400

    updated images again

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-mogrify -resize 400x400 source/img/*.JPG
-mogrify -resize 400x400 source/img/*.jpg
+mogrify -resize 400x400 source/images/*.JPG
+mogrify -resize 400x400 source/images/*.jpg
 middleman build
 cp -r build/* /usr/share/nginx/html/
