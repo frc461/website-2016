b1d963c58c35c92a9f281a336c1142659901c9ad
Brad Thompson <brad@boilerinvasion.org>
2016-10-19 12:51:31 -0400

    fixed contact info & added sponsor

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ git pull
 mogrify -resize 400x400 source/images/*.JPG
 mogrify -resize 400x400 source/images/*.jpg
 middleman build
-cp -r build/* /usr/share/nginx/html/
+cp -r build/* /var/www/html/
