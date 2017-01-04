5deddd5302298cf1737e9a4bbf6c2503c8d4eb4a
Sammidysam <sammidysam@gmail.com>
2017-01-03 19:54:38 -0500

    Indent everything

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,13 +1,13 @@
 //= require_tree .
 $(function() {
-var slick = $('.carousel').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    variableWidth: true,
-    centerMode: true,
-    centerPadding: '20px'
-});
+	var slick = $('.carousel').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		variableWidth: true,
+		centerMode: true,
+		centerPadding: '20px'
+	});
 });
